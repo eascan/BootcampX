a7c01e2a1fe4b4c02ed56e62eb9fb7d53be3259c
Edward <edward.ascan@me.com>
2021-02-04 05:08:09 +0000

    add parametized code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,26 +8,25 @@ const pool = new Pool({
 });
 
 const queryString = `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `;
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
 
 const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
 // Store all potentially malicious values in an array.
-const values = [`%${cohortName}%`, limit];
+const values = [cohortName];
 
 pool
   .query(queryString, values)
+
   .then((res) => {
     res.rows.forEach((user) => {
-
-      console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
-      );
+      console.log(`${user.cohort}: ${user.teacher}`);
     });
   })
   .catch((err) => console.error("query error", err.stack));
